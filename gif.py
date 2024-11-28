a9f4782be4e5fce0d11a48b04a4d66767019260c
Coolboomer9 <134468233+Coolboomer9@users.noreply.github.com>
2024-11-28 17:01:48 +0800

    Add files via upload

---
+++ gif.py
@@ -0,0 +1,33 @@
+import os
+from PIL import Image
+import imageio
+import tkinter as tk
+from tkinter import filedialog
+
+def select_folder():
+    root = tk.Tk()
+    root.withdraw()  
+    folder_path = filedialog.askdirectory()  
+    return folder_path
+
+def images_to_gif(image_folder, output_path, duration=0.5):
+    images = []
+    for file_name in sorted(os.listdir(image_folder)):
+        if file_name.endswith(('png', 'jpg', 'jpeg')):
+            file_path = os.path.join(image_folder, file_name)
+            images.append(imageio.imread(file_path))
+    
+    imageio.mimsave(output_path, images, duration=duration)
+    print(f"GIF saved to {output_path}")
+
+# 主程序
+if __name__ == "__main__":
+    print("Please select the folder containing the images")
+    image_folder = select_folder()
+    
+    if image_folder:
+        output_path = os.path.join(image_folder, 'output.gif')
+        duration = 1/16  # The time each picture is displayed in seconds
+        images_to_gif(image_folder, output_path, duration)
+    else:
+        print("No folder selected")
